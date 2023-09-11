c84f03efdaa2fd369440d46258908bf6f7ab06d2
Mark Douglas <mark.t.douglas@gmail.com>
2023-09-11 22:37:45 +0100

    Much faster way

--- 2021/06/main.cpp
+++ 2021/06/main.cpp
@@ -1,7 +1,33 @@
 #include "parser.hpp"
 #include "context.hpp"
 #include "scanner.hpp"
-#include <iomanip>
+
+long evolve(long initial, long ticks)
+{
+    long state[9], answer = 0, temp;
+
+    std::fill_n(state,9,0);
+
+    state[initial]++;
+
+    while(ticks--)
+    {
+        temp = state[0];
+        state[0] = state[1];
+        state[1] = state[2];
+        state[2] = state[3];
+        state[3] = state[4];
+        state[4] = state[5];
+        state[5] = state[6];
+        state[6] = state[7] + temp;
+        state[7] = state[8];
+        state[8] = temp;
+    }
+
+    for(std::size_t j = 0; j < 9; j++) answer += state[j];
+
+    return answer;
+}
 
 int main (int argc, char **argv)
 {
@@ -12,33 +38,20 @@ int main (int argc, char **argv)
 
     Parser.parse();
 
-    std::vector<long> today = Context.list, tomorrow;
-    long n_new;
+    long state[9], n, temp, answer;
+    const long ticks_1 = 18, ticks_2 = 80, ticks_3 = 256;
 
-    for(int i = 0; i < 256; i++)
-    {
-        tomorrow.clear();
-        n_new = 0;
+    answer = 0;
+    for(auto& i : Context.list) answer += evolve(i,ticks_1);
+    std::cout << answer << std::endl;
 
-        for(auto & i : today)
-        {
-            if(i == 0) {
-                n_new++;
-                tomorrow.push_back(6);
-            }
-            else {
-                tomorrow.push_back(i-1);
-            }
-        }
+    answer = 0;
+    for(auto& i : Context.list) answer += evolve(i,ticks_2);
+    std::cout << answer << std::endl;
 
-        while(n_new--) tomorrow.push_back(8);
-
-        today = tomorrow;
-        //for(auto & i : today) std::cout << std::setw(2) << i;
-        //std::cout << std::endl;
-    }
-
-    std::cout << today.size() << std::endl;
+    answer = 0;
+    for(auto& i : Context.list) answer += evolve(i,ticks_3);
+    std::cout << answer << std::endl;
 
     return 0;
 }
