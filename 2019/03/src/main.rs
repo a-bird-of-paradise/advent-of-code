a38878eff919ae56ae2e6cca7bc70ec246ca552a
Mark Douglas <mark.t.douglas@gmail.com>
2024-10-09 12:57:14 +0100

    Well that wasn't too bad. Use a HashMap to store (x,y) -> distance, and iterate over the points.

--- 2019/03/src/main.rs
+++ 2019/03/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 use std::env;
 use std::collections::BTreeSet;
@@ -20,23 +21,34 @@ fn main() {
         .map(|s| map_splitter(s))
         .collect();
 
-    let first_path = enpath(&first);
-    let second_path = enpath(&second);
+    let first_path: HashMap<(i64,i64), i64> = enpath(&first);
+    let second_path: HashMap<(i64,i64), i64> = enpath(&second);
 
-    let path_intersection : BTreeSet<(i64,i64)> = first_path.intersection(&second_path).cloned().collect();
+    let mut first_path_points : BTreeSet<(i64,i64)> = Default::default();
+    let mut second_path_points : BTreeSet<(i64,i64)> = Default::default();
+
+    for pt in &first_path { first_path_points.insert((pt.0.0,pt.0.1)); }
+    for pt in &second_path { second_path_points.insert((pt.0.0,pt.0.1)); }
+
+    let path_intersection : BTreeSet<(i64,i64)> = first_path_points.intersection(&second_path_points).cloned().collect();
 
     let mut answer_1 : i64 = std::i64::MAX;
+    let mut answer_2 : i64 = std::i64::MAX;
 
     for it in path_intersection {
         let mut x = it.0; 
         let mut y = it.1;
         if x == 0 && y == 0 { continue; }
+
+        if first_path[&(x,y)] + second_path[&(x,y)] < answer_2 { answer_2 = first_path[&(x,y)] + second_path[&(x,y)]; }
+
         if x < 0 { x = -x; }
         if y < 0 { y = -y; }
         if x + y < answer_1 { answer_1 = x + y; }
     }
 
     println!("{answer_1}");
+    println!("{answer_2}");
 }   
 
 fn map_splitter(s : &str) -> (char,i64)
@@ -46,13 +58,12 @@ fn map_splitter(s : &str) -> (char,i64)
     return (c,x);
 }
 
-fn enpath(def : &Vec<(char,i64)>) -> BTreeSet<(i64,i64)>
+fn enpath(def : &Vec<(char,i64)>) -> HashMap<(i64,i64), i64>
 {
     let mut x : i64 = 0;
     let mut y : i64 = 0; 
-    let mut answer : BTreeSet<(i64,i64)> = Default::default();
-
-    answer.insert((x,y));
+    let mut dist : i64 = 0;
+    let mut answer : HashMap<(i64,i64), i64> = HashMap::new();
 
     for it in def {
 
@@ -60,25 +71,29 @@ fn enpath(def : &Vec<(char,i64)>) -> BTreeSet<(i64,i64)>
             'L' =>  {   
                 for _i in 0..it.1 {
                     x = x - 1;
-                    answer.insert((x,y));
+                    dist = dist + 1;
+                    answer.insert((x,y),dist);
                 }
             }
             'R' =>  {   
                 for _i in 0..it.1 {
                     x = x + 1;
-                    answer.insert((x,y));
+                    dist = dist + 1;
+                    answer.insert((x,y),dist);
                 }
             }
             'U' =>  {   
                 for _i in 0..it.1 {
                     y = y + 1;
-                    answer.insert((x,y));
+                    dist = dist + 1;
+                    answer.insert((x,y),dist);
                 }
             }
             'D' =>   {   
                 for _i in 0..it.1 {
                     y = y - 1;
-                    answer.insert((x,y));
+                    dist = dist + 1;
+                    answer.insert((x,y),dist);
                 }
             }
             _ => panic!("Unknown move type")
